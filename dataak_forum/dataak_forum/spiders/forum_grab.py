001867e5c4fb60944e0465e7a1006a8f26f8144f
Mahan <mahan.abbuspour@gmail.com>
2020-02-06 21:02:19 +0330

    Fix login.

--- dataak_forum/dataak_forum/spiders/forum_grab.py
+++ dataak_forum/dataak_forum/spiders/forum_grab.py
@@ -1,29 +1,50 @@
 # -*- coding: utf-8 -*-
-import scrapy
-from scrapy.http import FormRequest
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.http import FormRequest, Request
+from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
+
 from ..items import DataakForumItem
 
 
-class ForumGrabSpider(scrapy.Spider):
+class ForumGrabSpider(CrawlSpider):
     name = "forum-grab"
     allowed_domains = ["forum.dataak.com"]
     user_agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 ' \
                  'Safari/537.36 '
 
-    # start_urls = (
-    #     'http://forum.dataak.com',
-    # )
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths='//td/strong/a')),
+        Rule(LinkExtractor(restrict_xpaths='//a[@class="pagination_next"])')),
+        Rule(LinkExtractor(restrict_xpaths='//span[@class=" subject_old"]/a'), follow=True, callback='parse_thread'),
+    )
 
     def start_requests(self):
         return [
-            FormRequest(
-                "http://forum.dataak.com/member.php?action=login",
-                formdata={'action': 'do_login',
-                          'quick_login': '1', 'my_post_key': 'ba5af38b9c7da114b0ad8181f56eb31b',
-                          'quick_username': 'mahan', 'quick_password': '@123456', 'quick_remember': 'yes',
-                          'submit': u'\u0648\u0631\u0648\u062f', 'url': 'http://forum.dataak.com/index.php'}
-            )]
+            Request(
+                'http://forum.dataak.com/member.php?action=login',
+                callback=self.parse_login
+
+            )
+        ]
+
+    def parse_login(self, response):
+        return FormRequest.from_response(
+            response,
+            formnumber=1,
+            formdata={'quick_username': 'mahan', 'quick_password': '@123456'},
+            callback=self.after_login
+        )
+
+    # def after_login(self, response):
+    #     with open('myfile.html', 'w') as f:
+    #         f.write(response.body)
+
+    def parse_thread(self, response):
+        pass
+
+
+
 
     def parse(self, response):
         item = ItemLoader(item=DataakForumItem(), response=response)
